680ca1a1f9501a06d50a40c6a58123d079970059
Paul Sheridan <paul@phobos-software.net>
2020-07-29 15:33:32 +0100

    chore: stub service for deployment

---
+++ src/stub-app/ag-test/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ag_test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public TestController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string site = _config.GetSection("Ident")["Name"];
+            return $"I Am {site}";
+        }
+    }
+}
